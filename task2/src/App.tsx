9e2427b2dee8723a130af1d1b0668e724562d371
Navro <tomeknawrocki2000@gmail.com>
2023-02-23 01:21:19 +0100

    Finished styles

--- task2/src/App.tsx
+++ task2/src/App.tsx
@@ -3,27 +3,81 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { TextField } from '@mui/material';
+import EditLocationIcon from '@mui/icons-material/EditLocation';
+import { Button, TextField } from '@mui/material';
+import { Container } from '@mui/system';
 
 function App() {
   return (
-    <div className="App">
-      <Accordion>
+    <Container>
+      <Accordion sx={{ 
+          backgroundColor: '#FFF9ED',
+          maxWidth: '500px',
+          transform: 'none',
+        }}>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
+          sx={{
+            transform: 'none',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>Personal data</Typography>
+          <Typography>Personal data</Typography>          
         </AccordionSummary>
-        <AccordionDetails>
-          <TextField id="standard-basic" label="Standard" variant="standard" />
-          <TextField id="standard-basic" label="Standard" variant="standard" />
-          <TextField id="standard-basic" label="Standard" variant="standard" />
+        <AccordionDetails           
+          sx={{
+            display: 'flex', 
+            alignItems: "center", 
+            justifyContent: "center", 
+            flexDirection: "column",
+          }}>
+          <TextField 
+            sx={{
+              width: '100%', 
+              marginBottom: "20px"
+            }} 
+            InputLabelProps={{ style: { color: '#F0740D' } }} 
+            InputProps={{ style: { color: '#F0740D' } }} 
+            id="standard-basic" 
+            label="Name" 
+            variant="standard" 
+          />
+         <TextField
+            id="standard-basic"
+            label="Email"
+            sx={{
+              width: '100%', 
+              marginBottom: "20px"
+            }} 
+            variant="standard"
+            InputLabelProps={{ style: { color: '#F0740D' } }}
+          />
+          <TextField 
+            sx={{
+              width: '100%', 
+              marginBottom: "20px"
+            }} 
+            id="standard-basic" 
+            InputLabelProps={{ style: { color: '#F0740D' } }} 
+            InputProps={{ style: { color: '#F0740D' } }} 
+            label="Password" 
+            variant="standard" 
+            type="password" 
+          />
+          <Button variant="text"
+            disableRipple={true}
+            sx={{
+              color: '#F0740D',
+            }}
+          >
+            SAVE
+          </Button>
         </AccordionDetails>
       </Accordion>
-    </div>
+    </Container>
   );
 }
 
